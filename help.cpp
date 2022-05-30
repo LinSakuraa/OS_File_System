16eda5ac6a6b8b78c42c85f493cc5507e292ccdf
memorieslc <88536439+memorieslc@users.noreply.github.com>
2022-05-30 18:08:44 +0800

    Directory.cpp add to project now
    Directory.cpp add to project now

---
+++ help.cpp
@@ -0,0 +1,41 @@
+//
+// Created by 19150 on 2022/5/30.
+//
+
+#include "help.h"
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+
+
+int statistic(const string& filename)
+{
+    ifstream fin(filename);
+    if(!fin.is_open())
+        return -1;
+    string t;
+    fin >> noskipws >> t;
+    return t.size();
+}
+
+extern string getCurrentTime()
+{
+    time_t now = time(0);
+    char *dt = ctime(&now);
+    string current_time = string(dt);
+    return current_time;
+}
+
+//获取本地电脑主机名，用来进行菜单显示
+extern string getLocalIpName() {
+    TCHAR buf[MAX_COMPUTERNAME_LENGTH + 2];
+    DWORD buf_size;
+    buf_size = sizeof buf - 1;
+    GetComputerName(buf, &buf_size);
+    string a = buf;
+    return a;
+}
