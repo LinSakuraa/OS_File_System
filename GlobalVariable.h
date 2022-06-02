ce9da1e3e238901eaa7184a0a7c90b2f0fd4da74
22325 <2232558142@qq.com>
2022-06-02 20:30:04 +0800

    update nv bug fix

--- GlobalVariable.h
+++ GlobalVariable.h
@@ -5,20 +5,20 @@
 #ifndef OS_BETA_GLOBALVARIABLE_H
 #define OS_BETA_GLOBALVARIABLE_H
 
-#define BLOCKSIZE 64 // 磁盘块大小
-#define DIRECTINDEXNUM 10 // 混合索引表中直接索引的数量
-#define INDEXTABLESIZE (BLOCKSIZE/4) // 普通索引表大小
-#define SECONDINDEXSIZE (INDEXTABLESIZE * INDEXTABLESIZE) // 二次间接索引表大小
-#define THIRDINDEXSIZE (SECONDINDEXSIZE * INDEXTABLESIZE) // 三次间接索引表大小
-#define MIXINDEXSIZE (DIRECTINDEXNUM + INDEXTABLESIZE + SECONDINDEXSIZE + THIRDINDEXSIZE) // 混合索引表大小
+#define BLOCKSIZE 64                                                                            // block size
+#define DIRECTINDEXNUM 10                                                                       // direct index num of mix index table
+#define INDEXTABLESIZE (BLOCKSIZE/4)                                                            // common index table size
+#define SECONDINDEXSIZE (INDEXTABLESIZE * INDEXTABLESIZE)                                       // second index size
+#define THIRDINDEXSIZE (SECONDINDEXSIZE * INDEXTABLESIZE)                                       // third index size
+#define MIXINDEXSIZE (DIRECTINDEXNUM + INDEXTABLESIZE + SECONDINDEXSIZE + THIRDINDEXSIZE)       // mix index size
 
-#define GROUPSIZE 50 // 成组链接法每一组的大小
-#define TOTALGROUPSIZE 500 // 成组链接法初始化时的总大小
+#define GROUPSIZE 50                                                                            // group size
+#define TOTALGROUPSIZE 500                                                                      // total group size
 
-#define INODENUM 128    // i结点个数
-#define MAXINODETNUM 64 // 内存中i结点表可容纳i结点的最大数量
-#define SYSTEMFILEMAXNUM 64  // 系统文件打开表最大数量
-#define USERFILEMAXNUM 5 // 用户文件打开表最大数量
+#define INODENUM 128                                                                            // number of inode
+#define MAXINODETNUM 64                                                                         // max inode num in ram
+#define SYSTEMFILEMAXNUM 64                                                                     // max system open table num
+#define USERFILEMAXNUM 5                                                                        // max user open table num
 
 
 #endif //OS_BETA_GLOBALVARIABLE_H
