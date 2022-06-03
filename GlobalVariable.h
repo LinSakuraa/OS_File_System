bc0f5f4223d5dc0230723d7c46ea7565eeef833e
22325 <2232558142@qq.com>
2022-06-03 14:38:22 +0800

    change global variable

--- GlobalVariable.h
+++ GlobalVariable.h
@@ -5,8 +5,8 @@
 #ifndef OS_BETA_GLOBALVARIABLE_H
 #define OS_BETA_GLOBALVARIABLE_H
 
-#define BLOCKSIZE 64                                                                            // block size
-#define DIRECTINDEXNUM 10                                                                       // direct index num of mix index table
+#define BLOCKSIZE 128                                                                            // block size
+#define DIRECTINDEXNUM 16                                                                     // direct index num of mix index table
 #define INDEXTABLESIZE (BLOCKSIZE/4)                                                            // common index table size
 #define SECONDINDEXSIZE (INDEXTABLESIZE * INDEXTABLESIZE)                                       // second index size
 #define THIRDINDEXSIZE (SECONDINDEXSIZE * INDEXTABLESIZE)                                       // third index size
